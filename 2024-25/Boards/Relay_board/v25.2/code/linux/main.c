d9be491133f06fe745ff98227462bcebf4709864
Arda Caliskan <zoomingguy2@proton.me>
2025-05-13 17:21:36 -0700

    Switched code to weak pull-ups

--- 2024-25/Boards/Relay_board/v25.2/code/linux/main.c
+++ 2024-25/Boards/Relay_board/v25.2/code/linux/main.c
@@ -5,17 +5,17 @@ static __code uint16_t __at(0x2007) configword1 = 0x00D4;
 
 // Pins 
 enum GpioBit {
-    BMS_fault_o,    //GP0 (Pin 7) - Inverted for BMS relay
-    BMS_gate_i,     //GP1 (Pin 6)
-    green_PIC_o,    //GP2 (Pin 5) - Blinks Not Faulted
+    BMS_fault_i,    //GP0 (Pin 7) - Inverted for BMS relay
+    IMD_gate_i,     //GP1 (Pin 6)
+    red_PIC_o,      //GP2 (Pin 5) - Blinks Not Faulted
     reserved, 
-    IMD_gate_i,     //GP4 (Pin 3) - Active low
-    red_PIC_o       //GP5 (Pin 2) - Blinks Faulted
+    BMS_fault_o,     //GP4 (Pin 3) - Active low
+    green_PIC_o      //GP5 (Pin 2) - Blinks Faulted
 };
 
 // --- Constants ---
 #define FREQ_SCALAR 10000 // Number of Timer1 overflows for half blink period
-                             // Approx: 30 * 8.192ms = 245.76ms (~2Hz blink rate)
+                          // Approx. 570ms, 1.75 Hz
 
 // --- Global Variables ---
 
@@ -28,11 +28,12 @@ int main(void) {
     CMCON0 = 0x07;
 
     //tristate register for i/o
-    TRISIO = (1 << BMS_gate_i) | (1 << IMD_gate_i) | (1 << 3); // Set inputs
+    TRISIO = (1 << BMS_fault_i) | (1 << IMD_gate_i) | (1 << 3); // Set inputs
     TRISIO &= ~((1 << BMS_fault_o) | (1 << green_PIC_o) | (1 << red_PIC_o)); // Clear outputs
 
     WPU = 0x00;
-    OPTION_REG = 0x80; // Disable global pull-ups
+    
+    OPTION_REG = 0x00;  // enable pull-ups
 
     GPIO &= ~((1 << BMS_fault_o) | (1 << green_PIC_o) | (1 << red_PIC_o));
 
@@ -50,15 +51,15 @@ int main(void) {
 
     //disable intreupts
     PIE1 &= ~(1 << TMR1IE);
-    INTCON &= ~((1 << GIE) | (1 << PEIE));
+    INTCON &= ~((1 << GIE) | (1 << PEIE)| (1 << 4));
 
     while (1) {
 
-        uint8_t bms_gate_state = (GPIO >> BMS_gate_i) & 1; //  GP1 
+        uint8_t bms_gate_state = (GPIO >> BMS_fault_i) & 1; //  GP1 
         uint8_t imd_gate_state = (GPIO >> IMD_gate_i) & 1; //  GP4 
 
-        uint8_t current_fault = (bms_gate_state == 0) || (imd_gate_state == 1); // coded to be backwards rn
-        
+        uint8_t current_fault = (bms_gate_state == 1) || (imd_gate_state == 0); 
+
         //inverted for BMS relay
         if (bms_gate_state) { 
             GPIO &= ~(1 << BMS_fault_o); // Set fault output LOW
@@ -67,9 +68,8 @@ int main(void) {
         }
 
 
-        //if (PIR1 & (1 << 0)) {  // TMR1F
-        //    PIR1 &= (1<<0);   // TMR1F
-        if (1){
+        if (PIR1 & (1 << 0)) {  // TMR1F
+            PIR1 &= (1<<0);   // TMR1F
             blink_counter++; // 8.2ms
 
             if (blink_counter >= FREQ_SCALAR) {
@@ -77,10 +77,14 @@ int main(void) {
 
                 
                 if (current_fault == 1) {
-                    GPIO ^= (1 << red_PIC_o);
+                    TRISIO ^= (1 << red_PIC_o); // turn on/off output
+                    WPU ^= (1 << red_PIC_o); // turn on/off pull-up
+                    GPIO ^= (1 << red_PIC_o);   // turn on/off output
                     GPIO &= ~(1 << green_PIC_o);
                 } else {
                     GPIO |= (1 << green_PIC_o);
+                    TRISIO &= ~(1 << red_PIC_o); // make an output
+                    WPU &= ~(1 << red_PIC_o);
                     GPIO &= ~(1 << red_PIC_o);
                 }
             }
